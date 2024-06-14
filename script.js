f0f733ca285476f98853ca765cd004a9d8e57600
aamirkhan06 <114560386+aamirkhan06@users.noreply.github.com>
2024-06-14 22:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	let strArr=s.split(" ");
+	return strArr[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
